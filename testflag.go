97c745134a0040fc40f7b047be73cf051738ff28
dreambee <zouying56@163.com>
2019-05-29 11:25:00 +0800

    1

--- testflag.go
+++ testflag.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"fmt"
 	"flag"
+	"bufio"
 )
 
 /**
@@ -43,7 +44,50 @@ func ParseFlag()  {
 
 }
 
+func ParseStdin()  {
+
+	//方式一
+	var str1 string
+
+	fmt.Print("Please input str1: ")
+	fmt.Scanf("%s\n", &str1)
+	fmt.Println("Output str1: ", str1)
+
+	fmt.Println("----------------------------")
+
+	var str2, str3 string
+	fmt.Print("Please input str2-str3: ")
+	fmt.Scanf("%s %s\n", &str2, &str3)
+
+	fmt.Println("Output str2-str3: ", str2, str3)
+
+	fmt.Println("----------------------------")
+
+	var str4, str5 string
+	fmt.Print("Please input str4-str5: ")
+	fmt.Scanf("%s %s", &str4, &str5)
+
+	fmt.Println("Output str4-str5: ", str4, str5)
+
+
+	//方式二
+	//reader := bufio.NewReader(os.Stdin)
+	//fmt.Println("用户名:")
+	//username ,_ := reader.ReadString('\n')
+	//fmt.Println("密码:")
+	//password ,_ := reader.ReadString('\n')
+	//
+	//fmt.Println("用户名和密码:",username,password)
+
+
+}
+
 func main()  {
 	//ParseOsArgs()
-	ParseFlag()
+	//ParseFlag()
+	//ParseStdin()
+
+
+
 }
+
